1688baa76ea57eb3a471260f507098a1a06a90f8
Fekleite <fernandacarolineleite360@gmail.com>
2021-02-23 21:11:39 -0300

    feat: create completed challenges component

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,11 @@
+import { Container } from "../styles/components/completedChallenges";
+
+export function CompletedChallenges() {
+  return (
+    <Container>
+      <span>Desafios completos</span>
+
+      <span>00</span>
+    </Container>
+  );
+}
