e76e78b2603ab4f6920c573c5b8d5586c1ca3a3b
Fekleite <fernandacarolineleite360@gmail.com>
2021-02-24 19:09:26 -0300

    feat: countdown hook

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,27 +1,88 @@
+import { useState, useEffect } from "react";
+
 import { CountdownContainer, CountdownButton } from "../styles/components/countdown";
 
+let countdownTimeout: NodeJS.Timeout;
+
 export function Countdown() {
+  const [time, setTime] = useState(0.1 * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [hasFinished, setHasFinished] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+  function startCountdown() {
+    setIsActive(true);
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout);
+    setIsActive(false);
+    setTime(25 * 60);
+  }
+
+  useEffect(() => {
+    if(isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1);
+      }, 1000)
+    } else if(isActive && time === 0) {
+      console.log("Finalizou");
+      setHasFinished(true);
+      setIsActive(false);
+    }
+  }, [isActive, time]);
+
   return (
     <div>
       <CountdownContainer>
         <div>
-          <span>2</span>
-          <span>5</span>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
         </div>
 
         <span>:</span>
 
         <div>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
         </div>
       </CountdownContainer>
 
-      <CountdownButton type="button">
-        Iniciar um ciclo
-
-        <img src="icons/play.svg" alt=""/>
-      </CountdownButton>
+      {
+        hasFinished ? (
+          <CountdownButton 
+            disabled
+          >
+            Ciclo encerrado
+          </CountdownButton>
+        ) : (
+          <>
+            {
+              isActive ? (
+                <CountdownButton 
+                  type="button" 
+                  onClick={resetCountdown}
+                  buttonActive
+                >
+                  Abandonar ciclo
+                </CountdownButton>
+              ) : (
+                <CountdownButton 
+                  type="button" 
+                  onClick={startCountdown}
+                >
+                  Iniciar um ciclo
+                </CountdownButton>
+              )
+            }
+          </>
+        )
+      }
     </div>
   );
 }
--- src/styles/components/countdown.ts
+++ src/styles/components/countdown.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+interface ButtonCountdownProps {
+  buttonActive?: boolean;
+}
+
 export const CountdownContainer = styled.div`
   display: flex;
   align-items: center;
@@ -37,7 +41,7 @@ export const CountdownContainer = styled.div`
   }
 `;
 
-export const CountdownButton = styled.button`
+export const CountdownButton = styled.button<ButtonCountdownProps>`
   width: 100%;
   height: 5rem;
   margin-top: 2rem;
@@ -46,17 +50,21 @@ export const CountdownButton = styled.button`
   justify-content: center;
   border: 0;
   border-radius: 5px;
-  background: var(--blue);
-  color: var(--white);
+  background: ${props => props.buttonActive ? "var(--white)" : "var(--blue)" };
+  color: ${props => props.buttonActive ? "var(--text)" : "var(--white)" };
   font-size: 1.25rem;
   font-weight: 600;
   transition: background-color 0.2s;
 
-  img {
-    margin-left: 8px;
+  &:disabled {
+    background: var(--white);
+    color: var(--text);
+    border-bottom: 4px solid var(--green);
+    cursor: not-allowed;
   }
 
-  &:hover {
-    background: var(--blue-dark);
+  &:not(:disabled):hover {
+    background: ${props => props.buttonActive ? "var(--red)" : "var(--blue-dark)" };
+    color: var(--white);
   }
 `;
